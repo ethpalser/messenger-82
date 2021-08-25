3cfdec9888a6d7f18bf17127e4c40de31d3d273b
ethpalser <eth.palser@gmail.com>
2021-08-25 12:09:39 -0400

    Connected Postgres Database
    - Connected Postgres database on local machine using .env files.
    - updated .gitignore to ignore .idea files generated by IDE (IntelliJ)

--- server/db/db.js
+++ server/db/db.js
@@ -1,7 +1,11 @@
 const Sequelize = require("sequelize");
 
-const db = new Sequelize(process.env.DATABASE_URL || "postgres://localhost:5432/messenger", {
-  logging: false
+const db = new Sequelize(
+    process.env.DATABASE,
+    process.env.DATABASE_USER,
+    process.env.DATABASE_PASSWORD, {
+  host: 'localhost',
+  dialect: 'postgres'
 });
 
 module.exports = db;
